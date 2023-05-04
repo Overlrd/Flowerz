f9c5db9c62aba2272fda91d1297c6a531e03de5c
Overlrd <premieraugust28@gmail.com>
2023-05-04 09:58:14 +0000

    m

--- Flowerz/settings.py
+++ Flowerz/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os 
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +31,8 @@ MODEL_CONFIG_PATH = os.path.join(MODEL_ASSETS_ROOT, 'model_config.json')
 MODEL_WEIGHTS_PATH = os.path.join(MODEL_ASSETS_ROOT, 'model_weights.h5')
 MODEL_LABELS_PATH = os.path.join(MODEL_ASSETS_ROOT, "label_name_dict.pickle")
 
+# Load environnement variables 
+TREFFLE_API_KEY = os.getenv("TREFFLE_API_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
