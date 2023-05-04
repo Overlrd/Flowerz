1b6c305a79ce8ac54acc5034910bc74c53b668d8
Overlrd <premieraugust28@gmail.com>
2023-05-04 09:55:06 +0000

    Using mocked objects for test

--- Flowerz/tests.py
+++ Flowerz/tests.py
@@ -1,23 +1,42 @@
+from unittest.mock import patch, MagicMock
 from django.test import TestCase
-from Flowerz.utils.treffle_wrapper.treffle import TreffleAPIWrapper , Query
+from Flowerz.utils.treffle_wrapper.treffle import TreffleAPIWrapper, Query
 from Flowerz.settings import TREFFLE_API_KEY
+
+
 class TreffleAPIWrapperTestCase(TestCase):
     def setUp(self):
         self.token = TREFFLE_API_KEY
         self.API = TreffleAPIWrapper(self.token)
 
-    def test_search_species(self):
+    @patch('Flowerz.utils.treffle_wrapper.treffle.TreffleAPIWrapper.make_request')
+    def test_search_species(self, mock_make_request):
+        # Mock the response of the API
+        mock_make_request.return_value = {
+            "data": [{
+                "common_name": "Passion flower",
+                "scientific_name": "Passiflora mixta"
+            }]
+        }
+
         query = "passion flower"
-        filters = {"scientific_name":"Passiflora mixta"}
+        filters = {"scientific_name": "Passiflora mixta"}
         request = self.API.search_plants(query=query).filter_(**filters).build()
-        print(request)
         result = self.API.make_request(*request)
         self.assertEqual(result["data"][0]["common_name"], "Passion flower")
 
+    @patch('Flowerz.utils.treffle_wrapper.treffle.TreffleAPIWrapper.make_request')
+    def test_search_plants(self, mock_make_request):
+        # Mock the response of the API
+        mock_make_request.return_value = {
+            "data": [
+                {"common_name": "Plant 1"},
+                {"common_name": "Plant 2"},
+                {"common_name": "Plant 3"}
+            ]
+        }
 
-    def test_search_plants(self):
         query = "passion flower"
         request = self.API.search_plants(query, limit=3).build()
-        print(request)
         result = self.API.make_request(*request)
         self.assertEqual(len(result["data"]), 3)
