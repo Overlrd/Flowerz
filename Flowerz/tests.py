4efff5c7b11b5ae24d9ff2ed4de1bc456a989bde
Overlrd <premieraugust28@gmail.com>
2023-05-04 09:49:10 +0000

    updated , tested TreffleAPIWrapper

--- Flowerz/tests.py
+++ Flowerz/tests.py
@@ -1,24 +1,23 @@
 from django.test import TestCase
-from Flowerz.utils.treffle_wrapper.treffle import TreffleAPIWrapper
-
+from Flowerz.utils.treffle_wrapper.treffle import TreffleAPIWrapper , Query
+from Flowerz.settings import TREFFLE_API_KEY
 class TreffleAPIWrapperTestCase(TestCase):
     def setUp(self):
-        self.token = "fb7c8Funa_gZnYU5onH0Oj79uapv-vvUMZ9tDqU0JTo"
-        self.wrapper = TreffleAPIWrapper(self.token)
+        self.token = TREFFLE_API_KEY
+        self.API = TreffleAPIWrapper(self.token)
 
     def test_search_species(self):
         query = "passion flower"
-        # scientific name , common_name value is case sensitive
         filters = {"scientific_name":"Passiflora mixta"}
-        result = self.wrapper.search_species(query, filters=filters)
-        print(result)
-        print()
+        request = self.API.search_plants(query=query).filter_(**filters).build()
+        print(request)
+        result = self.API.make_request(*request)
         self.assertEqual(result["data"][0]["common_name"], "Passion flower")
 
 
     def test_search_plants(self):
         query = "passion flower"
-        result = self.wrapper.search_plants(query, limit=3)
-        print(result)
-        print()
+        request = self.API.search_plants(query, limit=3).build()
+        print(request)
+        result = self.API.make_request(*request)
         self.assertEqual(len(result["data"]), 3)
