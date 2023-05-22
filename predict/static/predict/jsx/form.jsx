dd063a57751ee057d89371a8955763f9b2020aed
Overlrd <premieraugust28@gmail.com>
2023-05-22 08:11:50 +0000

    refactoring templates

---
+++ predict/static/predict/jsx/form.jsx
@@ -0,0 +1,6 @@
+const Myform = () => {
+  return (
+    <div>myform</div>
+  )
+}
+export default Myform
---
+++ predict/static/predict/jsx/index.jsx
@@ -0,0 +1,6 @@
+import Myform from "./form";
+
+const app = document.getElementById('root');
+
+
+ReactDOM.render(<Myform ></Myform>, app);
